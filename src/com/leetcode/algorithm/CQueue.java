f48355480dabce952e9e999fef37ac3c72b6ca5c
zhanghefei <18363992217@163.com>
2020-08-02 15:27:34 +0800

    add stack

---
+++ src/com/leetcode/algorithm/CQueue.java
@@ -0,0 +1,38 @@
+package com.leetcode.algorithm;
+
+import java.util.Stack;
+
+public class CQueue {
+
+    /** stack1用于添加元素 */
+    private Stack<Integer> stack1;
+
+    /** stack2用于删除元素 */
+    private Stack<Integer> stack2;
+
+    public CQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    public void appendTail(int value) {
+        if (stack1.empty() && !stack2.empty()){
+            while (!stack2.empty()){
+                stack1.push(stack2.pop());
+            }
+        }
+        stack1.push(value);
+    }
+
+    public int deleteHead() {
+        if (!stack1.empty() && stack2.empty()){
+            while (!stack1.empty()){
+                stack2.push(stack1.pop());
+            }
+        }
+        if (stack2.empty()){
+            return -1;
+        }
+        return stack2.pop();
+    }
+}
