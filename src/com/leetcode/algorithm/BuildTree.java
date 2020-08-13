92a659247c98eb4e0b45595cb256d837b697e217
zhanghefei <18363992217@163.com>
2020-08-13 23:33:49 +0800

    重构二叉树  优化内存和速度

--- src/com/leetcode/algorithm/BuildTree.java
+++ src/com/leetcode/algorithm/BuildTree.java
@@ -33,6 +33,9 @@ import java.util.Map;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  *
  * 解法：递归
+ * 解析：（1）前序遍历得第一个数字一定是根节点，该数字在中序遍历中的位置是根的位置，在该位置得左侧是根得左子树，在该位置得右侧是根得右子树
+ * （2）因此可以分别知道左子树总数n和右子树得总数m，在前序遍历中根节点后的n数字是左子树，左子树后边得m个数字是右子树
+ * （3）当左子树（右子树）得总数量=0，则左子树为为空；=1，则只有一个节点；>1,则有多个节点，递归构建剩下得树
  */
 public class BuildTree {
 
@@ -47,46 +50,42 @@ public class BuildTree {
 
     public TreeNode buildTree(int[] preorder, int[] inorder){
 
-        if (preorder.length == 0){
+        if (preorder == null || preorder.length == 0){
             return null;
         }
 
-        // 使用map记录中序遍历中每个值的下标
+        // 使用map保存中序遍历中每个节点的下标
         Map<Integer, Integer> inorderMap = new HashMap<>(inorder.length);
         for (int i = 0; i < inorder.length; i ++){
             inorderMap.put(inorder[i], i);
         }
 
-        // 前序遍历中的第一个元素必定是二叉树的根
-        TreeNode node = new TreeNode(preorder[0]);
+        TreeNode root = new TreeNode(preorder[0]);
+        root.left = buildTree(preorder, 1, inorderMap.get(preorder[0]), inorder, 0, inorderMap.get(preorder[0]) - 1, inorderMap);
+        root.right = buildTree(preorder, inorderMap.get(preorder[0]) + 1, preorder.length - 1, inorder, inorderMap.get(preorder[0]) + 1, inorder.length - 1, inorderMap);
+        return root;
+    }
 
-        // 获取二叉树的根在中序遍历中的位置，在跟左侧数量是所有左子树的节点数，在根右侧数量是所有右子树的节点数
-        int nodeIndex = inorderMap.get(node.val);
+    private TreeNode buildTree(int[] preorder, int preorderStart, int preorderEnd,
+                               int[] inorder, int inorderStart, int inorderEnd,
+                               Map<Integer, Integer> inorderMap){
 
-        // 如果左子树总结点数为0，则左子树为null；为1，则只有一个节点；大于1，则递归左子树
-        if (nodeIndex == 0){
-            node.left = null;
-        }else if (nodeIndex == 1){
-            node.left = new TreeNode(preorder[1]);
-        }else{
-            int[] subPreorder = new int[nodeIndex];
-            int[] subInorder = new int[nodeIndex];
-            System.arraycopy(preorder, 1, subPreorder, 0, nodeIndex);
-            System.arraycopy(inorder, 0, subInorder, 0, nodeIndex);
-            node.left = buildTree(subPreorder, subInorder);
-        }
-
-        if (nodeIndex == inorder.length - 1){
-            node.right = null;
-        }else if (nodeIndex == inorder.length - 2){
-            node.right = new TreeNode(preorder[preorder.length - 1]);
+        if (preorderStart > preorderEnd){
+            return null;
+        }else if (preorderStart == preorderEnd){
+            return new TreeNode(preorder[preorderStart]);
         }else {
-            int[] subPreorder = new int[inorder.length - 1 - nodeIndex];
-            int[] subInorder = new int[inorder.length - 1 - nodeIndex];
-            System.arraycopy(preorder, nodeIndex + 1, subPreorder, 0, inorder.length - 1 - nodeIndex);
-            System.arraycopy(inorder, nodeIndex  + 1, subInorder, 0, inorder.length - 1 - nodeIndex);
-            node.right = buildTree(subPreorder, subInorder);
+            TreeNode root = new TreeNode(preorder[preorderStart]);
+            root.left = buildTree(preorder, preorderStart + 1, inorderMap.get(preorder[preorderStart]) - inorderStart + preorderStart, inorder, inorderStart, inorderMap.get(preorder[preorderStart]) - 1, inorderMap);
+            root.right = buildTree(preorder, inorderMap.get(preorder[preorderStart]) - inorderStart + 1 + preorderStart, preorderEnd, inorder, inorderMap.get(preorder[preorderStart]) + 1, inorderEnd, inorderMap);
+            return root;
         }
-        return node;
+
+    }
+
+    public static void main(String... args) {
+        int[] preorder = new int[]{3,9,20,15,7};
+        int[] inorder = new int[]{9,3,15,20,7};
+        TreeNode root = new BuildTree().buildTree(preorder, inorder);
     }
 }
